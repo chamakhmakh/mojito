61dfc4a8f77df897d35cb841346dc99e9d2a2cce
farouk <bensaidf232@gmail.com>
2025-08-09 03:08:35 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./component/NavBAR.JSX";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-4xl text-red-900">hello world</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
